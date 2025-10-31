d472dbf50be662529099a0b13eea2958a9aca1f3
Ellaiza Mae Lontok <emae.lontok@gmail.com>
2025-10-31 22:17:48 +0800

    Update encryptor.py to fix encryption.

--- core/encryptor.py
+++ core/encryptor.py
@@ -1,17 +1,21 @@
-from config.morse_caesar_config import full_charset, morse_code_dict
+from config.morse_caesar_config import morse_code_dict
 
 def encrypt(original_text, shift_amount):
     encrypted_text = ""
+    morse_values_list = list(morse_code_dict.keys())
+
     for char in original_text:
-        if char in full_charset:
-            position = full_charset.index(char) + shift_amount
-            position %= len(full_charset)
-            encrypted_text += full_charset[position]
+        if char == ' ':
+            encrypted_text += ' '
+        elif char in morse_values_list:
+            position = morse_values_list.index(char) + shift_amount
+            position %= len(morse_values_list)
+            encrypted_text += morse_values_list[position]
         else:
             encrypted_text += char
 
     morse_text = []
-    for char in encrypted_text.upper():
+    for char in encrypted_text:
         morse_char = morse_code_dict.get(char, char)
         morse_text.append(morse_char)
     morse_result = " ".join(morse_text)
