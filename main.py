afaee8054c212093edf0ac5048bdc0cf8fde1cbd
Ellaiza Mae Lontok <emae.lontok@gmail.com>
2025-10-31 22:20:56 +0800

    Update main.py to improve interaction.

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from core.encryptor import encrypt
 from core.decryptor import decrypt
-from utils.display import print_invalid, print_exit, print_encrypted, print_decrypted
+from utils.display import print_invalid, print_exit, print_encrypted, print_decrypted, art
+
+print(art)
+print("Welcome to Morsear 🔐")
+print("Encode or decode your hidden messages with Caesar Cipher and Morse code.")
 
 exit_program = False
 while not exit_program:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").strip().lower()
+    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt: ").strip().lower()
 
     if direction == "encode":
-        string_input = input("Type the message you want to encrypt with Morsear: ")
+        string_input = input("Type the message you want to encrypt with Morsear: ").upper()
         shift_num = int(input("Type the shift number: "))
         encrypted_text = encrypt(string_input, shift_num)
         print_encrypted(encrypted_text)
-
     elif direction == "decode":
         morsear_input = input("Type your Morsear message to decrypt: ")
         shift_num = int(input("Type the shift number: "))
         decrypted_text = decrypt(morsear_input, shift_num)
         print_decrypted(decrypted_text)
-
     else:
         print_invalid()
 
